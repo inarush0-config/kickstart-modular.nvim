d19c74c6f513caa6a7f700042283c543b3cac768
Andrew Rush <inarush0@gmail.com>
2024-03-09 11:14:10 -0500

    added chezmoi plugin

---
+++ lua/custom/plugins/chezmoi-nvim.lua
@@ -0,0 +1,21 @@
+return {
+  'xvzc/chezmoi.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    require('chezmoi').setup {
+      -- your configurations
+      edit = {
+        watch = true,
+        force = false,
+      },
+      notification = {
+        on_open = true,
+        on_apply = true,
+        on_watch = false,
+      },
+      telescope = {
+        select = { '<CR>' },
+      },
+    }
+  end,
+}
