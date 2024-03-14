dbeb2c5b054f91b841064e2adf9fa672f7fb2696
Andrew Rush <rushndr@amazon.com>
2024-03-14 14:20:43 -0400

    updated Neoformat to run on BufWritePre

--- lua/custom/plugins/neoformat.lua
+++ lua/custom/plugins/neoformat.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     local aug_id = vim.api.nvim_create_augroup('neoformat', { clear = true })
     vim.g.neoformat_enabled_markdown = { 'mdformat' }
-    vim.api.nvim_create_autocmd('BufWritePost', {
+    vim.api.nvim_create_autocmd('BufWritePre', {
       pattern = { '*.md' },
       command = 'Neoformat',
     })
