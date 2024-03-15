cbc6bf20bc18d2f9bb534ccf24f8adf5f6a0dc75
Andrew Rush <rushndr@amazon.com>
2024-03-15 14:45:00 -0400

    enabled spelling, sorted options list

--- lua/options.lua
+++ lua/options.lua
@@ -3,9 +3,8 @@
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
 vim.opt.breakindent = true
+vim.opt.colorcolumn = '80'
 vim.opt.cursorline = true
 vim.opt.ignorecase = true
 vim.opt.inccommand = 'split'
@@ -15,15 +14,18 @@ vim.opt.mouse = 'a'
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 999
+vim.opt.shiftwidth = 4
 vim.opt.showmode = false
 vim.opt.signcolumn = 'yes'
 vim.opt.smartcase = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+vim.opt.tabstop = 4
 vim.opt.termguicolors = true
 vim.opt.timeoutlen = 300
 vim.opt.undofile = true
 vim.opt.updatetime = 250
-vim.opt.colorcolumn = "80"
+vim.opt.spelllang = 'en_us'
+vim.opt.spell = true
 
 -- vim: ts=2 sts=2 sw=2 et
