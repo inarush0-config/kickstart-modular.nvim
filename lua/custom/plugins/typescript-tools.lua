9b2711854710bb3d7b4d03001801b5a7f6b0a1d1
Andrew Rush <rushndr@amazon.com>
2024-03-31 21:51:40 -0400

    enabled typescript code actions

--- lua/custom/plugins/typescript-tools.lua
+++ lua/custom/plugins/typescript-tools.lua
@@ -2,4 +2,11 @@ return {
   'pmizio/typescript-tools.nvim',
   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
   opts = {},
+  config = function()
+    require('typescript-tools').setup {
+      settings = {
+        expose_as_code_action = 'all',
+      },
+    }
+  end,
 }
