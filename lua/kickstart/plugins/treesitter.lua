dd12b6005b8f9784af7ace16689880b8fc5395f2
Andrew Rush <inarush0@gmail.com>
2024-04-07 19:47:52 -0400

    setup treesitter-context

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -37,5 +37,14 @@ return {
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    config = function()
+      require('treesitter-context').setup()
+      vim.api.nvim_set_hl(0, 'TreesitterContext', { fg = 'fg', bg = 'bg' })
+      vim.cmd 'hi TreesitterContextBottom gui=underline'
+    end,
+  },
+  { 'nvim-treesitter/nvim-treesitter-textobjects' },
 }
 -- vim: ts=2 sts=2 sw=2 et
