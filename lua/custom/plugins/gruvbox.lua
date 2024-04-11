9ac6f073f38f331358ca6b8a0cf12b6433dba337
Andrew Rush <rushndr@amazon.com>
2024-04-11 09:36:45 -0400

    switched colorscheme source

--- lua/custom/plugins/gruvbox.lua
+++ lua/custom/plugins/gruvbox.lua
@@ -1,8 +1,9 @@
 return {
-  'morhetz/gruvbox',
+  'ellisonleao/gruvbox.nvim',
   lazy = false,
   priority = 1000,
   config = function()
+    vim.o.background = 'dark'
     vim.cmd.colorscheme 'gruvbox'
   end,
 }
