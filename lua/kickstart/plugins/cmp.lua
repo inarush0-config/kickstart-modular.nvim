1a9639827c4409fcef0519b2851877ba50cd88af
Andrew Rush <rushndr@amazon.com>
2024-05-03 13:38:21 -0400

    updated completion config and results priorities

--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -19,26 +19,51 @@ return {
         end)(),
       },
       'saadparwaiz1/cmp_luasnip',
-
-      -- Adds other completion capabilities.
-      --  nvim-cmp does not ship with all sources by default. They are split
-      --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
-
-      -- If you want to add a bunch of pre-configured snippets,
-      --    you can use this plugin to help you. It even has snippets
-      --    for various frameworks/libraries/etc. but you will have to
-      --    set up the ones that are useful for you.
-      -- 'rafamadriz/friendly-snippets',
+      'lukas-reineke/cmp-under-comparator',
     },
     config = function()
       -- See `:help cmp`
+      local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
       local cmp = require 'cmp'
       local luasnip = require 'luasnip'
       luasnip.config.setup {}
       require('luasnip.loaders.from_vscode').lazy_load()
 
+      local lspkind_comparator = function(conf)
+        local lsp_types = require('cmp.types').lsp
+        return function(entry1, entry2)
+          if entry1.source.name ~= 'nvim_lsp' then
+            if entry2.source.name == 'nvim_lsp' then
+              return false
+            else
+              return nil
+            end
+          end
+          local kind1 = lsp_types.CompletionItemKind[entry1:get_kind()]
+          local kind2 = lsp_types.CompletionItemKind[entry2:get_kind()]
+          if kind1 == 'Variable' and entry1:get_completion_item().label:match '%w*=' then
+            kind1 = 'Parameter'
+          end
+          if kind2 == 'Variable' and entry2:get_completion_item().label:match '%w*=' then
+            kind2 = 'Parameter'
+          end
+
+          local priority1 = conf.kind_priority[kind1] or 0
+          local priority2 = conf.kind_priority[kind2] or 0
+          if priority1 == priority2 then
+            return nil
+          end
+          return priority2 < priority1
+        end
+      end
+
+      local label_comparator = function(entry1, entry2)
+        return entry1.completion_item.label < entry2.completion_item.label
+      end
+
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
       cmp.setup {
         snippet = {
           expand = function(args)
@@ -89,19 +114,47 @@ return {
         sources = {
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
-          { name = 'nvim_lsp_signature_help' },
           { name = 'nvim_lua' },
           { name = 'path' },
         },
-        -- sorting = {
-        --   comparators = {
-        --     cmp.config.compare.score,
-        --     cmp.config.compare.locality,
-        --     cmp.config.compare.recently_used,
-        --     cmp.config.compare.offset,
-        --     cmp.config.compare.order,
-        --   },
-        -- },
+        sorting = {
+          comparators = {
+            require('cmp-under-comparator').under,
+            cmp.config.compare.scope,
+            lspkind_comparator {
+              kind_priority = {
+                Parameter = 14,
+                Variable = 12,
+                Field = 11,
+                Property = 11,
+                Constant = 10,
+                Enum = 10,
+                EnumMember = 10,
+                Event = 10,
+                Function = 10,
+                Method = 10,
+                Operator = 10,
+                Reference = 10,
+                Struct = 10,
+                File = 8,
+                Folder = 8,
+                Class = 5,
+                Color = 5,
+                Module = 5,
+                Keyword = 2,
+                Constructor = 1,
+                Interface = 1,
+                Snippet = 0,
+                Text = 1,
+                TypeParameter = 1,
+                Unit = 1,
+                Value = 1,
+              },
+            },
+            label_comparator,
+            cmp.config.compare.score,
+          },
+        },
       }
     end,
   },
