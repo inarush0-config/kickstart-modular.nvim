d40d6deed883de24d806a16b1e4e1331089df84a
Andrew Rush <rushndr@amazon.com>
2024-06-19 14:35:15 -0400

    updated telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -20,6 +20,25 @@ return {
       --  All the info you're looking for is in `:help telescope.setup()`
       --
       defaults = {
+        vimgrep_arguments = {
+          'rg',
+          '--follow', -- Follow symbolic links
+          '--hidden', -- Search for hidden files
+          '--no-heading', -- Don't group matches by each file
+          '--with-filename', -- Print the file path with the matched lines
+          '--line-number', -- Show line numbers
+          '--column', -- Show column numbers
+          '--smart-case', -- Smart case search
+
+          -- Exclude some patterns from search
+          '--glob=!**/.git/*',
+          '--glob=!**/.idea/*',
+          '--glob=!**/.vscode/*',
+          '--glob=!**/build/*',
+          '--glob=!**/dist/*',
+          '--glob=!**/yarn.lock',
+          '--glob=!**/package-lock.json',
+        },
         file_ignore_patterns = {
           '.DS_Store',
           '.git/',
