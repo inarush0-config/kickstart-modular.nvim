96800658c4a4127a9fcbd12252199864d143b0a1
Andrew Rush <rushndr@amazon.com>
2024-09-03 12:45:53 -0400

    default mason config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -163,12 +163,7 @@ return {
       },
     }
 
-    require('mason').setup {
-      providers = {
-        'mason.providers.client',
-        'mason.providers.registry-api',
-      },
-    }
+    require('mason').setup {}
 
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
