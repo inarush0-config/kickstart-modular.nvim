62ccefc5c0d30848f0dc25515819e94fae95b285
Andrew Rush <rushndr@amazon.com>
2024-09-03 12:37:22 -0400

    cleaned up lsp/mason config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -176,11 +176,11 @@ return {
       'prettier',
       'stylua',
       'taplo',
-      'golangci-lint',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
     require('mason-lspconfig').setup {
+      automatic_installation = true,
       handlers = {
         function(server_name)
           local server = servers[server_name] or {}
