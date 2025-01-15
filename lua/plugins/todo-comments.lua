5d30f750d916bf8f250fc7c4465dd426bb5d79d1
Andrew Rush <rushndr@amazon.com>
2025-01-15 09:10:14 -0500

    changed todo's lazy event

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,6 +1,6 @@
 return {
   'folke/todo-comments.nvim',
-  event = 'VimEnter',
+  event = { 'BufReadPost', 'BufNewFile' }, -- Lazy-load on file events
   dependencies = { 'nvim-lua/plenary.nvim' },
   opts = { signs = false },
 }
