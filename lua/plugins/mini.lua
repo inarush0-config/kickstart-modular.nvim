bfec344b3cedfe38d808fdb8b7f6fa1a0c23fbf6
Andrew Rush <rushndr@amazon.com>
2025-01-23 15:54:58 -0500

    restored autopairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -8,7 +8,6 @@ return {
       require('mini.align').setup {}
       require('mini.comment').setup {}
       require('mini.indentscope').setup {}
-      require('mini.pairs').setup {}
       require('mini.surround').setup {}
       require('mini.move').setup {}
     end,
