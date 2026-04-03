6ae2b85cf8d0b9b30ab92f1123a9c447f56f0a90
Andrew Rush <inarush0@gmail.com>
2026-04-03 12:08:57 -0400

    restore removed Lsp commands

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -144,4 +144,22 @@ return {
       ensure_installed = vim.tbl_keys(servers),
     }
   end,
+
+  -- Restore removed Lsp commands
+  vim.api.nvim_create_user_command('LspInfo', 'checkhealth vim.lsp', {
+    desc = 'Show LSP Info',
+  }),
+
+  vim.api.nvim_create_user_command('LspLog', function(_)
+    local state_path = vim.fn.stdpath 'state'
+    local log_path = vim.fs.joinpath(state_path, 'lsp.log')
+
+    vim.cmd(string.format('edit %s', log_path))
+  end, {
+    desc = 'Show LSP log',
+  }),
+
+  vim.api.nvim_create_user_command('LspRestart', 'lsp restart', {
+    desc = 'Restart LSP',
+  }),
 }
