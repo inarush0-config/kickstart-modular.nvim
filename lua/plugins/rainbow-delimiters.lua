3c9a45871490e6253a29bdddc8a0ae4add8dd6b2
Andrew Rush <andrew.rush@dbhds.virginia.gov>
2026-08-12 13:19:08 -0400

    fixed rainbow delimiters config

--- lua/plugins/rainbow-delimiters.lua
+++ lua/plugins/rainbow-delimiters.lua
@@ -1,16 +1,12 @@
 return {
-  'HiPhish/rainbow-delimiters.nvim',
-  opts = {
-    strategy = { 'global' },
-  },
-  config = function(_, opts)
-    local ok, rainbow_delimiters = pcall(require, 'rainbow-delimiters')
-    if not ok then
-      vim.notify('Failed to load rainbow-delimiters.nvim', vim.log.levels.ERROR)
-      return
-    end
-
-    rainbow_delimiters.setup(opts)
-  end,
+    'HiPhish/rainbow-delimiters.nvim',
     event = 'VeryLazy',
+    config = function()
+        -- These are the plugin’s configuration variables
+        vim.g.rainbow_delimiters = {
+            strategy = {
+                [''] = require('rainbow-delimiters').strategy['global'],
+            },
+        }
+    end,
 }
