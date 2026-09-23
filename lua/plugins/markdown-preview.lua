16092995ac5804a0aa230fd0e38c46587e8e2531
Andrew Rush <inarush0@gmail.com>
2026-09-23 12:48:27 -0400

    updated to maintained markdown preview

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,9 +1,5 @@
 return {
-  'iamcco/markdown-preview.nvim',
+  'sammaji/markdown-preview.nvim',
   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-  build = 'cd app && npm install && git restore .',
-  init = function()
-    vim.g.mkdp_filetypes = { 'markdown' }
-  end,
   ft = { 'markdown' },
 }
